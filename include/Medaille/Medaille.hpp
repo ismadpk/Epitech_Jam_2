5b49a115ad043f09ba42f7fb9ae50c02ed148764
ismaïel <ismaiel.diapaka@epitech.eu>
2024-05-19 12:26:48 +0200

    [ADD]:  enum for know if it s water or fire in collision

--- include/Medaille/Medaille.hpp
+++ include/Medaille/Medaille.hpp
@@ -4,21 +4,28 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+enum Status {
+    Fire = 0,
+    Water
+};
+
 class Medaille {
     public:
         Medaille();
         void setTexture();
         void moveMedal(bool isCollision);
         void setPosition(sf::Vector2f pos);
-        sf::Vector2f getPosition();
-        sf::Vector2u getSize();
-        sf::Sprite getMedal();
+        sf::Vector2f getPosition() const;
+        sf::Vector2u getSize() const;
+        sf::Sprite getMedal() const;
+        Status getStatus() const;
         ~Medaille();
 
     private:
         sf::Texture _fireTexture;
         sf::Texture _waterTexture;
         sf::Sprite _medal;
+        Status _status;
 };
 
 #endif /* !MEDAILLE_HPP_ */
--- src/Medaille/Medaille.cpp
+++ src/Medaille/Medaille.cpp
@@ -7,14 +7,20 @@ Medaille::Medaille()
 
 void Medaille::setTexture()
 {
+    int posX = 0;
+
     if (!this->_fireTexture.loadFromFile("assets/sprites/FlameFaling.png"))
         return;
     this->_fireTexture.setSmooth(true);
 
     if (!this->_waterTexture.loadFromFile("assets/sprites/WaterDrop.png"))
         return;
+    std::srand(std::time(0));
+    posX = std::rand() % (1920 + 1 - 0) + 0;
     this->_waterTexture.setSmooth(true);
+    this->_medal.setPosition(posX, -80);
     this->_medal.setTexture(this->_fireTexture);
+    this->_status = Status::Fire;
 }
 
 void Medaille::moveMedal(bool isCollision)
@@ -23,23 +29,22 @@ void Medaille::moveMedal(bool isCollision)
     int posX = this->_medal.getPosition().x;
     std::srand(std::time(0));
     int random_value = std::rand() % 2;
-    sf::IntRect waterRect({0, 0, 60, 100});
 
     if (posY >= 850 || isCollision == true) {
         posY = -80;
         if (random_value == 0) {
+            sf::IntRect waterRect({0, 0, 60, 100});
             this->_medal.setTextureRect(waterRect);
             this->_medal.setTexture(this->_waterTexture);
-        }
-        else
+            this->_status = Status::Water;
+        } else {
+            sf::IntRect fireRect({0, 0, 32, 82});
+            this->_medal.setTextureRect(fireRect);
             this->_medal.setTexture(this->_fireTexture);
-            random_value = std::rand() % 3;
-        if (random_value == 0)
-            posX = 140;
-        else if (random_value == 1)
-            posX = 320;
-        else
-            posX = 560;
+            this->_status = Status::Fire;
+        }
+        std::srand(std::time(0));
+        posX = std::rand() % (1920 + 1 - 0) + 0;
     }
     this->_medal.setPosition(posX, posY);
 }
@@ -49,19 +54,24 @@ void Medaille::setPosition(sf::Vector2f pos)
     this->_medal.setPosition(pos.x, pos.y);
 }
 
-sf::Sprite Medaille::getMedal()
+sf::Sprite Medaille::getMedal() const
 {
     return this->_medal;
 }
 
-sf::Vector2u Medaille::getSize()
+sf::Vector2u Medaille::getSize() const
 {
     return this->_medal.getTexture()->getSize();
 }
 
-sf::Vector2f Medaille::getPosition()
+sf::Vector2f Medaille::getPosition() const
 {
     return this->_medal.getPosition();
+}
+
+Status Medaille::getStatus() const
+{
+    return this->_status;
 }
 
 Medaille::~Medaille()
